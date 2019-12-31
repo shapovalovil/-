6ac9ea041f87fca8514e3787cd53238bee22a06b
Iosif <shapovalov.iosif@yandex.ru>
2019-12-31 10:03:57 +0300

    first commit

---
+++ script_t.js
@@ -0,0 +1,29 @@
+'use strict';
+
+ /* var number = 100;
+ var numb;
+ var string = "Hellow";
+ var sym = Symbol();
+ var boolean = true;
+ null; //обращение к несуществующей переменной
+ undefined; //неопределённое значение переменной
+ var obj = {};
+
+ let myObj = {
+     name: "fog",
+     age: 23
+ };
+
+ let arr = ('wew', 'wew', 'wewew');
+ console.log(myObj.age);
+ console.log(myObj["name"]);
+ console.log(arr[1]);*/
+ //let answer = confirm("Что происходит??");
+let answer = +prompt("Что происходит??", "Никогда");
+ console.log(answer);
+ let x = 10; 
+ console.log(x++);
+ console.log(x);
+
+ let y,z = 10; z = y;
+ console.log(y===z);
